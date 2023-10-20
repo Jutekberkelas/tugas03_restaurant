c3da81e7b4ca9b81dd3e75abf827f64c7a9407a7
Mirna Al Aushofy <114629808+Jutekberkelas@users.noreply.github.com>
2023-10-20 10:27:21 +0700

    Add files via upload

---
+++ restaurant.js
@@ -0,0 +1,30 @@
+const restaurant = [
+    {
+      name: "seefood  Resto",
+      city: "Jakarta",
+      favoriteDrink: "Es kopi",
+      favoriteFood: "Kepiting Saus Tiram",
+      isVegan: true
+    },
+    {
+      name: "Saung kuring",
+      city: "Bandung",
+      favoriteDrink: "Es Campur",
+      favoriteFood: "Ayam Bakar",
+      isVegan: false
+    },
+    {
+      name: "Tidak Ada",
+      city: "Bogor",
+      favoriteDrink: "Air putih",
+      favoriteFood: "Seblak",
+      isVegan: true
+    }
+  ];
+
+  console.log(restaurant);
+
+  const name = ("By: Mirna Kamilah || Ti22A")
+
+  console.log(name);
+  
